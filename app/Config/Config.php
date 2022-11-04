beb2f33d51691aa4d292ab388fbc2073a708a8dc
runzboi <richesmetelewawon@gmail.com>
2022-11-04 10:15:46 +0100

    edited config file

--- app/Config/Config.php
+++ app/Config/Config.php
@@ -3,13 +3,13 @@
 define('APPROOT',dirname(dirname(__FILE__)) );
 
 // Url Root
-define('URLROOT', 'http://localhost/mvc');
+define('URLROOT', '__yourUrlRoot__');
 
 // Url SITENAME
-define('SITENAME', 'RichesMvc');
+define('SITENAME', '__YourSiteName__');
 
 // Db params
 define("DB_HOST","localhost");
-define("DB_USER","root");
-define("DB_PWD",'');
-define("DB_NAME","mvc");
+define("DB_USER","__YOUR_DB_USER__");
+define("DB_PWD",'__YOUR_DB_PASSWORD__');
+define("DB_NAME","__YOUR_DB_NAME__");
